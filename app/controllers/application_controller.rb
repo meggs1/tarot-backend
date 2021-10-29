f3b1c7e0ca54910dd77eac273c23d710d8a9f3c4
megan <megan.schertz@gmail.com>
2021-10-28 23:56:44 -0500

    decoded_token actually returning token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::API
 
   def decoded_token
     if auth_header
-      token = auth_header.split(' ')[1]
-      # header: { 'Authorization': 'Bearer <token>' }
+      token = auth_header.split(' ')[0]
       begin
         JWT.decode(token, ENV["JWT_SECRET_KEY"], true, algorithm: 'HS256')
       rescue JWT::DecodeError
+
         nil
       end
     end
@@ -27,8 +27,6 @@ class ApplicationController < ActionController::API
     if decoded_token
       user_id = decoded_token[0]['user_id']
       @user = User.find_by(id: user_id)
-    else
-      nil
     end
   end
 
