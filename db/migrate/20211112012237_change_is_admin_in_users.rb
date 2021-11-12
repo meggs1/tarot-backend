3f992a4e1e9675fb95a7a8568ef224847328b762
megan <megan.schertz@gmail.com>
2021-11-11 23:16:31 -0600

    first Heroku deploy

--- db/migrate/20211112012237_change_is_admin_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIsAdminInUsers < ActiveRecord::Migration[6.1]
-  def change
-    change_column :users, :is_admin, :boolean , :default => false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_012237) do
+ActiveRecord::Schema.define(version: 2021_11_07_041801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 2021_11_12_012237) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_admin", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
