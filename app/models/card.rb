d8148e6d8ab3d9b35fa8fe32067f2373fa32eecd
megan <megan.schertz@gmail.com>
2021-11-12 17:37:44 -0600

    update serializer

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,7 +10,7 @@ class Card < ApplicationRecord
     has_attached_file :avatar
 
     def image_url
-        url_for(self.image_file)
+        self.avatar.url
     end
 
     # def picture_from_url(url)
