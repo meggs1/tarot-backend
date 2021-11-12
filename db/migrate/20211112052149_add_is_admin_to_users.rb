7d3dfb4f22715d8d8f4bdf8044b1b20691eebbe5
megan <megan.schertz@gmail.com>
2021-11-11 23:36:19 -0600

    first Heroku deploy

--- db/migrate/20211112052149_add_is_admin_to_users.rb
+++ db/migrate/20211112052149_add_is_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddIsAdminToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :first_visit, :boolean, :default => true
+    add_column :users, :is_admin, :boolean, :default => false
   end
 end
