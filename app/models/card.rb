0939bf2c2ae7dac08a3f1a220afd6f9f70d9c24a
megan <megan.schertz@gmail.com>
2021-11-16 20:36:43 -0600

    still trying to get up images in seed

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,12 +6,15 @@ class Card < ApplicationRecord
     belongs_to :arcana
 
     has_one_attached :image_file
+    
 
     has_attached_file :avatar
 
     def image_url
         url_for(self.image_file)
     end
+
+    validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
     # def picture_from_url(url)
     #     self.picture = URI.parse(url)
