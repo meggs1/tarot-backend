679d228b0045be639e1f2162c8f1bcec464143c4
megan <megan.schertz@gmail.com>
2021-11-16 19:09:40 -0600

    still trying to get up images in seed

--- app/models/card.rb
+++ app/models/card.rb
@@ -9,9 +9,9 @@ class Card < ApplicationRecord
 
     has_attached_file :avatar
 
-    def image_url
-        url_for(self.image_file)
-    end
+    # def image_url
+    #     url_for(self.image_file)
+    # end
 
     # def picture_from_url(url)
     #     self.avatar = open(url)
