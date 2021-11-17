89827286dc39f61c9fab2e60fd3415bccd0ad22d
megan <megan.schertz@gmail.com>
2021-11-16 21:03:02 -0600

    D:

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,17 +8,17 @@ class Card < ApplicationRecord
     has_one_attached :image_file
     
 
-    has_attached_file :avatar
+    # has_attached_file :avatar
 
     def image_url
         url_for(self.image_file)
     end
 
-    validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
-    # def picture_from_url(url)
-    #     self.picture = URI.parse(url)
+    # def avatar_from_url(url)
+    #     self.avatar = URI.parse(url).open
     # end
+
+    # validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
     
 end
  
